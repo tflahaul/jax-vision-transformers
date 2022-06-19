418056e767c9dbecc73dfa38074d23e2cf08b3d7
Thomas Flahault <thomas.pro@tutanota.com>
2022-06-19 12:31:06 +0200

    [vit] needs pre-norm, dropout & more tests

---
+++ vit.py
@@ -0,0 +1,54 @@
+from flax import linen as nn
+from jax import numpy as jnp
+
+class Residual(nn.Module):
+	func: nn.Module
+
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		return self.func(inputs) + inputs
+
+class FeedForward(nn.Module):
+	dim: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		out = nn.Dense(self.dim)(inputs)
+		out = nn.relu(out)
+		out = nn.Dense(inputs.shape[-1])(out)
+		return out
+
+class MultiHeadAttention(nn.Module):
+	dim: int
+	num_heads: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		B, L, E = inputs.shape
+		out = nn.Dense(self.num_heads * self.dim * 3, use_bias=False)(inputs)
+		q, k, v = [x.reshape(B, L, self.num_heads, self.dim).transpose((0, 2, 1, 3)) for x in out.split(3, -1)]
+		out = nn.softmax(jnp.matmul(q, k.transpose((0, 1, 3, 2))) * (1 / jnp.sqrt(self.dim)))
+		out = jnp.matmul(out, v).transpose((0, 2, 1, 3)).reshape(B, L, -1)
+		out = nn.Dense(E, use_bias=False)(out)
+		return out
+
+class ViT(nn.Module):
+	patch_size: int
+	out_features: int
+	dim: int = 192
+	depth: int = 12
+	num_heads: int = 12
+	dim_heads: int = 768
+	dim_ff: int = 3072
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		P = self.patch_size
+		out = nn.Conv(self.dim, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
+		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, L, E]
+		out = nn.Sequential([
+			*(nn.Sequential([
+				Residual(MultiHeadAttention(self.dim_heads, self.num_heads)),
+				Residual(FeedForward(self.dim_ff))
+			]) for d in range(self.depth))])(out)
+		out = nn.Dense(self.out_features)(out)
+		return out
