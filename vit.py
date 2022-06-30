1c668191f534b3f5880a65d9ce7d3935f6fd600d
Thomas Flahault <thomas.pro@tutanota.com>
2022-06-30 16:36:23 +0200

    [vit] residual block w/ pre layernorm, position embedding

--- vit.py
+++ vit.py
@@ -1,19 +1,19 @@
 from flax import linen as nn
 from jax import numpy as jnp
 
-class Residual(nn.Module):
+class ResidualPreNorm(nn.Module):
 	func: nn.Module
 
+	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
-		return self.func(inputs) + inputs
+		return self.func(nn.LayerNorm(epsilon=1e-9)(inputs)) + inputs
 
 class FeedForward(nn.Module):
 	dim: int
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
-		out = nn.LayerNorm(epsilon=1e-9)(inputs)
-		out = nn.Dense(self.dim)(out)
+		out = nn.Dense(self.dim)(inputs)
 		out = nn.gelu(out)
 		out = nn.Dense(inputs.shape[-1])(out)
 		return out
@@ -25,10 +25,9 @@ class MHDPAttention(nn.Module):
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
 		B, L, E = inputs.shape
-		out = nn.LayerNorm(epsilon=1e-9)(inputs)
-		out = nn.Dense(self.num_heads * self.dim * 3, use_bias=False)(out)
+		out = nn.Dense(self.num_heads * self.dim * 3, use_bias=False)(inputs)
 		q, k, v = [x.reshape(B, L, self.num_heads, self.dim).transpose((0, 2, 1, 3)) for x in out.split(3, -1)]
-		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) * (1 / jnp.sqrt(self.dim)))
+		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(self.dim))
 		out = jnp.matmul(out, v).transpose((0, 2, 1, 3)).reshape(B, L, -1)
 		out = nn.Dense(E, use_bias=False)(out)
 		return out
@@ -47,10 +46,12 @@ class ViT(nn.Module):
 		P = self.patch_size
 		out = nn.Conv(self.dim, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
 		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, L, E]
+		pe = self.param('pe', nn.initializers.uniform(), (1, *out.shape[-2:])) # position embedding
+		out = out + pe
 		out = nn.Sequential([
 			*(nn.Sequential([
-				Residual(MHDPAttention(self.dim_heads, self.num_heads)),
-				Residual(FeedForward(self.dim_ff))
+				ResidualPreNorm(MHDPAttention(self.dim_heads, self.num_heads)),
+				ResidualPreNorm(FeedForward(self.dim_ff))
 			]) for d in range(self.depth))])(out)
 		out = nn.Dense(self.out_features)(out)
 		return out
