46dc360171bb1c805c87b7399293e4e5c18efe10
Thomas Flahault <thomas.pro@tutanota.com>
2022-07-03 10:56:26 +0200

    [vit] kaiming he init for class token & pos embedding

--- vit.py
+++ vit.py
@@ -39,24 +39,24 @@ class ViT(nn.Module):
 	depth: int
 	num_heads: int
 	dim_heads: int
-	dim_ff: int
+	dim_ffn: int
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
 		P = self.patch_size
 		out = nn.Conv(self.dim, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
 		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, L, E]
-		ct = self.param('ct', nn.initializers.uniform(), (1, 1, self.dim))
-		pe = self.param('pe', nn.initializers.uniform(), (1, out.shape[1] + 1, out.shape[2]))
+		ct = self.param('ct', nn.initializers.he_uniform(), (1, 1, self.dim))
+		pe = self.param('pe', nn.initializers.he_uniform(), (1, out.shape[1] + 1, out.shape[2]))
 		out = jnp.concatenate((ct.repeat(out.shape[0], 0), out), axis=1)
 		out = out + pe
 		out = nn.Sequential([
 			*(nn.Sequential([
 				ResidualPreNorm(MHDPAttention(self.dim_heads, self.num_heads)),
-				ResidualPreNorm(FeedForward(self.dim_ff))
+				ResidualPreNorm(FeedForward(self.dim_ffn))
 			]) for d in range(self.depth))])(out)
 		out = out[:, 0]
-		out = nn.Dense(self.dim_ff)(out)
+		out = nn.Dense(self.dim_ffn)(out)
 		out = nn.tanh(out)
 		out = nn.Dense(self.out_features)(out)
 		return out
