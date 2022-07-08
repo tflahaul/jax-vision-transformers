33d9e3949a2d3ddae0454ed4de2c1c94bb7be66e
Thomas Flahault <thomas.pro@tutanota.com>
2022-07-08 19:02:09 +0200

    [vit] heads dim is ViT width; initializers

--- vit.py
+++ vit.py
@@ -19,15 +19,14 @@ class FeedForward(nn.Module):
 		return out
 
 class MHDPAttention(nn.Module):
-	dim: int
 	num_heads: int
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
 		B, L, E = inputs.shape
-		out = nn.Dense(self.num_heads * self.dim * 3, use_bias=False)(inputs)
-		q, k, v = [x.reshape(B, L, self.num_heads, self.dim).transpose((0, 2, 1, 3)) for x in out.split(3, -1)]
-		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(self.dim))
+		out = nn.Dense(self.num_heads * E * 3, use_bias=False)(inputs)
+		q, k, v = (x.reshape(B, L, self.num_heads, E).transpose((0, 2, 1, 3)) for x in out.split(3, -1))
+		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(E))
 		out = jnp.matmul(out, v).transpose((0, 2, 1, 3)).reshape(B, L, -1)
 		out = nn.Dense(E, use_bias=False)(out)
 		return out
@@ -35,23 +34,22 @@ class MHDPAttention(nn.Module):
 class ViT(nn.Module):
 	patch_size: int
 	out_features: int
-	dim: int
+	width: int
 	depth: int
-	num_heads: int
-	dim_heads: int
+	num_heads: int    
 	dim_ffn: int
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
 		P = self.patch_size
-		out = nn.Conv(self.dim, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
+		out = nn.Conv(self.width, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
 		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, L, E]
-		ct = self.param('ct', nn.initializers.he_uniform(), (1, 1, self.dim))
-		pe = self.param('pe', nn.initializers.he_uniform(), (1, out.shape[1] + 1, self.dim))
-		out = jnp.concatenate((ct.repeat(out.shape[0], 0), out), axis=1)
+		pe = self.param('pe', nn.initializers.normal(0.02), (1, out.shape[1] + 1, self.width))
+		ct = self.param('ct', nn.initializers.zeros, (1, 1, self.width))
+		out = jnp.concatenate((ct.repeat(out.shape[0], axis=0), out), axis=1)
 		out = out + pe
 		out = nn.Sequential([*(nn.Sequential([
-				ResidualPreNorm(MHDPAttention(self.dim_heads, self.num_heads)),
+				ResidualPreNorm(MHDPAttention(self.num_heads)),
 				ResidualPreNorm(FeedForward(self.dim_ffn))
 			]) for d in range(self.depth))])(out)
 		out = out[:, 0]
