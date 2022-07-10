828560474b38ec6e8c3b7510251ea1e496fa4c6f
Thomas Flahault <thomas.pro@tutanota.com>
2022-07-10 20:03:59 +0200

    [deit] https://arxiv.org/abs/2012.12877

---
+++ deit.py
@@ -0,0 +1,60 @@
+from flax import linen as nn
+from jax import numpy as jnp
+
+class ResidualPreNorm(nn.Module):
+	func: nn.Module
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		return self.func(nn.LayerNorm()(inputs)) + inputs
+
+class FeedForward(nn.Module):
+	dim: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		out = nn.Dense(self.dim)(inputs)
+		out = nn.gelu(out)
+		out = nn.Dense(inputs.shape[-1])(out)
+		return out
+
+class MHDPAttention(nn.Module):
+	num_heads: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		B, L, E = inputs.shape
+		out = nn.Dense(self.num_heads * E * 3, use_bias=True)(inputs)
+		q, k, v = (x.reshape(B, L, self.num_heads, E).transpose((0, 2, 1, 3)) for x in out.split(3, -1))
+		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(E))
+		out = jnp.matmul(out, v).transpose((0, 2, 1, 3)).reshape(B, L, -1)
+		out = nn.Dense(E, use_bias=False)(out)
+		return out
+
+class DeiT(nn.Module):
+	patch_size: int
+	out_features: int
+	width: int
+	depth: int
+	num_heads: int    
+	dim_ffn: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		P = self.patch_size
+		out = nn.Conv(self.width, kernel_size=(P, P), strides=P, use_bias=False)(inputs)
+		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, L, E]
+		pe = self.param('pe', nn.initializers.normal(0.02), (1, out.shape[1] + 2, self.width))
+		ct = self.param('ct', nn.initializers.zeros, (1, 1, self.width)) # class token
+		dt = self.param('dt', nn.initializers.zeros, (1, 1, self.width)) # distillation token
+		out = jnp.concatenate((ct.repeat(out.shape[0], 0), out, dt.repeat(out.shape[0], 0)), axis=1)
+		out = out + pe
+		out = nn.Sequential([*(nn.Sequential([
+				ResidualPreNorm(MHDPAttention(self.num_heads)),
+				ResidualPreNorm(FeedForward(self.dim_ffn))
+			]) for d in range(self.depth))])(out)
+		cls = nn.Dense(self.dim_ffn)(out[:, 0])
+		cls = nn.tanh(cls)
+		cls = nn.Dense(self.out_features)(cls)
+		dis = nn.Dense(self.out_features)(out[:, -1])
+		return cls, dis
