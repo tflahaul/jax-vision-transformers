a2d2ead00d8b4c21af642f6f3f400c1071dd1043
Thomas Flahault <thomas.pro@tutanota.com>
2022-07-11 22:02:58 +0200

    [mae] refactoring!

--- mae.py
+++ mae.py
@@ -1,6 +1,7 @@
-from flax import linen as nn
 from jax import random as jrnd
 from jax import numpy as jnp
+from flax import linen as nn
+from typing import Sequence
 
 class ResidualPreNorm(nn.Module):
 	func: nn.Module
@@ -20,17 +21,70 @@ class FeedForward(nn.Module):
 		return out
 
 class MHDPAttention(nn.Module):
-	dim: int
 	num_heads: int
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
 		B, L, E = inputs.shape
-		out = nn.Dense(self.num_heads * self.dim * 3, use_bias=False)(inputs)
-		q, k, v = [x.reshape(B, L, self.num_heads, self.dim).transpose((0, 2, 1, 3)) for x in out.split(3, -1)]
-		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(self.dim))
+		out = nn.Dense(self.num_heads * E * 3, use_bias=False)(inputs)
+		q, k, v = (x.reshape(B, L, self.num_heads, E).transpose((0, 2, 1, 3)) for x in out.split(3, -1))
+		out = nn.softmax(jnp.matmul(q, jnp.moveaxis(k, 2, 3)) / jnp.sqrt(E))
 		out = jnp.matmul(out, v).transpose((0, 2, 1, 3)).reshape(B, L, -1)
 		out = nn.Dense(E, use_bias=False)(out)
+		return out
+
+class MaskedEncoder(nn.Module):
+	key: jrnd.KeyArray
+	patch_size: int
+	masking_ratio: float
+	depth: int
+	width: int
+	num_heads: int
+	dim_ffn: int
+
+	@nn.compact
+	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
+		P = self.patch_size
+		out = nn.Conv(self.width, (P, P), strides=P, use_bias=False)(inputs)
+		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, P, E]
+		pe = self.param('pe', nn.initializers.normal(0.02), (1, out.shape[1] + 1, self.width))
+		ct = self.param('ct', nn.initializers.zeros, (1, 1, self.width)) # class token
+		out = jnp.concatenate((ct.repeat(out.shape[0], 0), out), axis=1)
+		out = out + pe
+		mask = jrnd.permutation(self.key, jnp.arange(1, out.shape[1] + 1))
+		tokens = out[:, mask[:int(out.shape[1] * (1 - self.masking_ratio))]]
+		tokens = nn.Sequential([*(nn.Sequential([
+				ResidualPreNorm(MHDPAttention(self.num_heads)),
+				ResidualPreNorm(FeedForward(self.dim_ffn))
+			]) for d in range(self.depth))])(tokens)
+		return tokens, mask, out.shape
+
+class MaskedDecoder(nn.Module):
+	inputs_shape: Sequence[int]
+	embeddings_shape: Sequence[int]
+	masking_ratio: float
+	patch_size: int
+	depth: int
+	width: int
+	num_heads: int
+	dim_ffn: int
+
+	@nn.compact
+	def __call__(self, patches: jnp.DeviceArray, mask: jnp.DeviceArray) -> jnp.DeviceArray:
+		B, N, E = self.embeddings_shape
+		mt = self.param('mt', nn.initializers.normal(0.02), (1, 1, E)) # mask token
+		pe = self.param('pe', nn.initializers.normal(0.02), (1, N, E))
+		out = mt.repeat(B, 0).repeat(N, 1)
+		out = out.at[:, mask[:int(N * (1 - self.masking_ratio))]].set(patches)
+		out = out + pe
+		out = nn.Dense(self.width)(out)
+		out = nn.Sequential([*(nn.Sequential([
+				ResidualPreNorm(MHDPAttention(self.num_heads)),
+				ResidualPreNorm(FeedForward(self.dim_ffn))
+			]) for d in range(self.depth))])(out)
+		out = out[:, 1:] # remove extra class token
+		out = nn.Dense((self.patch_size ** 2) * self.inputs_shape[-1])(out)
+		out = out.reshape(self.inputs_shape)
 		return out
 
 class MAE(nn.Module):
@@ -40,7 +94,6 @@ class MAE(nn.Module):
 	depth: int
 	width: int
 	num_heads: int
-	dim_heads: int
 	dim_ffn: int
 	dec_depth: int
 	dec_width: int
@@ -49,34 +102,21 @@ class MAE(nn.Module):
 
 	@nn.compact
 	def __call__(self, inputs: jnp.DeviceArray) -> jnp.DeviceArray:
-		B, H, W, C = inputs.shape
-		P = self.patch_size
-		out = nn.Conv(self.width, (P, P), strides=P, use_bias=False)(inputs)
-		out = out.reshape(out.shape[0], -1, out.shape[3]) # [B, H, W, E] -> [B, P, E]
-		ct = self.param('ct', nn.initializers.normal(0.02), (1, 1, self.width)) # class token
-		epe = self.param('epe', nn.initializers.he_uniform(), (1, out.shape[1] + 1, self.width))
-		out = jnp.concatenate((ct.repeat(out.shape[0], 0), out), axis=1)
-		out = out + epe
-
-		mask = jrnd.permutation(self.key, jnp.arange(1, out.shape[1] + 1))
-		patches = out[:, mask[:int(out.shape[1] * (1 - self.masking_ratio))]]
-		patches = nn.Sequential([*(nn.Sequential([
-				ResidualPreNorm(MHDPAttention(self.dim_heads, self.num_heads)),
-				ResidualPreNorm(FeedForward(self.dim_ffn))
-			]) for d in range(self.depth))])(patches)
-
-		mt = self.param('mt', nn.initializers.normal(0.02), (1, 1, self.width)) # mask token
-		dpe = self.param('dpe', nn.initializers.he_uniform(), (1, out.shape[1] + 1, self.width))
-		out = mt.repeat(out.shape[0], 0).repeat(out.shape[1], 1)
-		out = out.at[:, mask[:int(out.shape[1] * (1 - self.masking_ratio))]].set(patches)
-		out = out + dpe
-
-		out = nn.Dense(self.dec_width)(out)
-		out = nn.Sequential([*(nn.Sequential([
-				ResidualPreNorm(MHDPAttention(self.dec_width, self.dec_heads)),
-				ResidualPreNorm(FeedForward(self.dec_ffn_dim))
-			]) for d in range(self.depth))])(out)
-		out = out[:, 1:] # remove extra class token
-		out = nn.Dense(P * P * C)(out)
-		out = out.reshape(B, H, W, C)
+		tokens, mask, embeddings_shape = MaskedEncoder(
+			self.key,
+			self.patch_size,
+			self.masking_ratio,
+			self.depth,
+			self.width,
+			self.num_heads,
+			self.dim_ffn)(inputs)
+		out = MaskedDecoder(
+			inputs.shape,
+			embeddings_shape,
+			self.masking_ratio,
+			self.patch_size,
+			self.dec_depth,
+			self.dec_width,
+			self.dec_heads,
+			self.dec_ffn_dim)(tokens, mask)
 		return out
