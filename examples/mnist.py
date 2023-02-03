1ee0696459d1ae02a1a5e2340d8ea3bac6515064
tflahaul <thomas.pro@tutanota.com>
2023-02-03 10:13:30 +0100

    modification of some hyper parameters

--- examples/mnist.py
+++ examples/mnist.py
@@ -10,14 +10,15 @@ from jax import numpy as jnp
 
 from jvt import ViT
 
-LEARNING_RATE = 0.01
+LEARNING_RATE = 2e-3
 MOMENTUM = 0.9
-MAX_ITER = 6
+MAX_ITER = 8
 BATCH_SIZE = 256
-CKPT_DIR = 'models'
+CKPT_DIR = 'checkpoints'
 
 train_set = tfds.as_numpy(tfds.load('mnist', split='train', batch_size=BATCH_SIZE, as_supervised=True, data_dir='/tmp'))
 test_set = tfds.as_numpy(tfds.load('mnist', split='test', batch_size=-1, as_supervised=True, data_dir='/tmp'))
+
 
 @jax.jit
 def apply_model(state, images, labels, key):
@@ -28,10 +29,12 @@ def apply_model(state, images, labels, key):
 	grad_fn = jax.value_and_grad(loss_fn)
 	return grad_fn(state.params)
 
+
 @partial(jax.jit, static_argnames='infer_fn')
 def accuracy(parameters, infer_fn) -> float:
 	images, labels = test_set
 	return jnp.mean(jnp.argmax(infer_fn(parameters, images), -1) == labels)
+
 
 def create_train_state(rng: jax.random.KeyArray, f: nn.Module):
 	parameters = jax.jit(f.init)(rng, jnp.ones((1, 28, 28, 1)))
@@ -41,10 +44,11 @@ def create_train_state(rng: jax.random.KeyArray, f: nn.Module):
 		params=parameters,
 		tx=optimizer)
 
+
 def main() -> None:
 	train_fn = ViT(28, 10, 48, 3, 6, 192, enable_dropout=True, dropout_rate=0.1)
 	infer_fn = ViT(28, 10, 48, 3, 6, 192, enable_dropout=False).apply
-	kp, kd = jax.random.split(jax.random.PRNGKey(seed=3407))
+	kp, kd = jax.random.split(jax.random.PRNGKey(seed=42))
 	state = create_train_state({'params': kp, 'dropout': kd}, train_fn)
 	print(f'Number of parameters: {sum(x.size for x in jax.tree_util.tree_leaves(state.params))}')
 	for epoch in range(1, MAX_ITER + 1):
@@ -57,6 +61,7 @@ def main() -> None:
 		acc = accuracy(state.params, infer_fn)
 		print(f'epoch {epoch:>2d}/{MAX_ITER}| loss={running_loss:.4f}, accuracy={acc:.3f}, time={time.time() - start:.2f}')
 	print(f'Saved {checkpoints.save_checkpoint(CKPT_DIR, state, 0, overwrite=True)}')
+
 
 if __name__ == '__main__':
 	main()
